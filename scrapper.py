63e839a1a8e698f6d6db5039a05ff252d80ec248
Alexandr Popov <grepto@gmail.com>
2019-12-05 17:17:32 +0300

    get_links

--- scrapper.py
+++ scrapper.py
@@ -1,8 +1,8 @@
-import re
 import random
+import re
 
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 
 USER_AGENT_LIST = [
     # Chrome
@@ -32,19 +32,19 @@ USER_AGENT_LIST = [
     'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; .NET CLR 2.0.50727; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729)'
 ]
 
-
 SCRAPPING_PREFERENCES = dict(
-    yandex = dict(
+    yandex=dict(
         url_pattern=lambda x: f'https://yandex.ru/search/?text={x}',
-        scrap_rule = lambda soup: [(a['href'], a.find('div', class_='organic__url-text').text) for a in
-                                soup.findAll('a', class_='organic__url', attrs={'data-bem': re.compile('^((?!video).)*$')})]
+        scrap_rule=lambda soup: [(a['href'], a.find('div', class_='organic__url-text').text) for a in
+                                 soup.findAll('a', class_='organic__url',
+                                              attrs={'data-bem': re.compile('^((?!video).)*$')})]
     ),
-    google = dict(
+    google=dict(
         url_pattern=lambda x: f'https://www.google.com/search?q={x}',
-        scrap_rule = lambda soup: [(div.a['href'], div.a.h3.span.text) for div in soup.find_all('div', class_='r')]
+        scrap_rule=lambda soup: [(div.a['href'], div.a.h3.span.text) for div in soup.find_all('div', class_='r')]
     ),
-    other = dict(
-        scrap_rule = lambda soup: [(a['href'], a.text) for a in soup.find_all('a', href=re.compile('^(http|https)://'))]
+    other=dict(
+        scrap_rule=lambda soup: [(a['href'], a.text) for a in soup.find_all('a', href=re.compile('^(http|https)://'))]
     ),
 )
 
@@ -52,12 +52,12 @@ SCRAPPING_PREFERENCES = dict(
 def get_search_url(domain, query):
     return SCRAPPING_PREFERENCES[domain]['url_pattern'](query)
 
+
 def get_scrapping_func(domain='other'):
     return SCRAPPING_PREFERENCES[domain]['scrap_rule']
 
 
 def get_links(url, scrapping_rule):
-
     user_agent = random.choice(USER_AGENT_LIST)
     headers = {'User-Agent': user_agent}
 
@@ -68,6 +68,7 @@ def get_links(url, scrapping_rule):
 
 
 def find_links(search_engine, query, limit=None, is_recursively=False):
+    print(search_engine, query, limit, is_recursively)
     url = get_search_url(search_engine, query)
     search_engine_scrapping_rule = get_scrapping_func(search_engine)
 
@@ -84,26 +85,3 @@ def find_links(search_engine, query, limit=None, is_recursively=False):
             result_list.extend(page_links)
 
     return result_list[:limit]
-
-
-
-
-url = 'https://www.google.com/search?q=рекурсия'
-url2 = 'https://yandex.ru/search/?text=дерево'
-url3 = 'https://ru.wikipedia.org/wiki/дерево'
-
-search_engine = 'google'
-query = 'лапоть'
-url = get_search_url(search_engine, 'лапоть')
-scrapping_rule = get_scrapping_func(search_engine)
-limit = 100
-is_recursively=True
-
-finded_links = find_links(search_engine, query, limit, is_recursively)
-
-print(*finded_links, sep='\n')
-
-
-# scrapping_rule = get_scrapping_func()
-# page_links = get_links('https://ru.wiktionary.org/wiki/%D0%BB%D0%B0%D0%BF%D0%BE%D1%82%D1%8C', scrapping_rule)
-# print(page_links)
